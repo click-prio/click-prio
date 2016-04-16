87c7e7ba651d8e27f54f718254c5bdec8922aea6
堀口誠人 <horiguchi_seito@liginc.co.jp>
2016-04-16 18:36:54 +0900

    onsenui

--- src/pc/js/app.js
+++ src/pc/js/app.js
@@ -1,14 +1,40 @@
 
 var app = angular.module("myApp", []);
 
+var USER_DATA = {};
+
 app.controller("myController", function () {
+
+    var count = 0;
+    var slider = document.getElementById('js-form-slider').getElementsByClassName('form__inner')[0];
 
     this.items = [
         {"name":"Dr.One" },
         {"name":"Dr.Two" },
         {"name":"Dr.Tree" }
     ];
+
+    this.moveSlide = function(){
+        count += 1;
+        slider.style.webkitTransform = 'translate3d(' + -count * window.innerWidth + 'px, 0,0';
+    };
+
+    this.selectDr = function(e){
+        USER_DATA.dr = e.target.value;
+        this.moveSlide();
+    };
+
+
+    //function clickHandler(self){
+    //    console.log($(self).val());
+    //}
+    //
+    //$('.js-data-button').on('click submit', function(e){
+    //    e.preventDefault();
+    //    clickHandler(this);
+    //});
 });
 
 require('./module/hack.js');
 require('./module/smooth-scroll.js');
+require('./module/data.js');
