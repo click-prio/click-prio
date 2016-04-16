9b00ce4a371a6da6d426b9111c212f36369f6d68
堀口誠人 <horiguchi_seito@liginc.co.jp>
2016-04-16 15:45:28 +0900

    filter

--- src/pc/js/app.js
+++ src/pc/js/app.js
@@ -1,2 +1,14 @@
+
+var app = angular.module("myApp", []);
+
+app.controller("myController", function () {
+
+    this.items = [
+        {"name":"Dr.One" },
+        {"name":"Dr.Two" },
+        {"name":"Dr.Tree" }
+    ];
+});
+
 require('./module/hack.js');
 require('./module/smooth-scroll.js');
