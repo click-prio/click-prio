befe3258c8a3d27470e565cde49553abf3d228d2
堀口誠人 <horiguchi_seito@liginc.co.jp>
2016-04-16 19:49:26 +0900

    slide move success

--- src/pc/js/app.js
+++ src/pc/js/app.js
@@ -8,12 +8,6 @@ app.controller("myController", function () {
     var count = 0;
     var slider = document.getElementById('js-form-slider').getElementsByClassName('form__inner')[0];
 
-    this.items = [
-        {"name":"Dr.One" },
-        {"name":"Dr.Two" },
-        {"name":"Dr.Tree" }
-    ];
-
     this.moveSlide = function(){
         count += 1;
         slider.style.webkitTransform = 'translate3d(' + -count * window.innerWidth + 'px, 0,0';
@@ -23,6 +17,19 @@ app.controller("myController", function () {
         USER_DATA.dr = e.target.value;
         this.moveSlide();
     };
+
+    this.doctors = [
+        {"name":"Dr.One" },
+        {"name":"Dr.Two" },
+        {"name":"Dr.Tree" }
+    ];
+
+    this.specialists = [
+        {"name":"Surgery" },
+        {"name":"Internal medicine" },
+        {"name":"Dentistry" },
+        {"name":"Department of Obstetrics and Gynecology" }
+    ];
 
 
     //function clickHandler(self){
